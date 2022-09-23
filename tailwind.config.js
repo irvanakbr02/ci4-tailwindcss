aa3613aa6d73ad17535faca912082ba5faa93bec
irvanakbr02 <irvanfebriansyah21@gmail.com>
2022-09-23 14:42:46 +0700

    codeigniter 4 & tailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/Views/*.php",
+    "./app/Views/**/*.php",
+    "./app/Views/**/**/*.php",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
